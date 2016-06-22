b826683743b510ac07b6408038f6f8f1bc9ddb5d
n Anthony Kim <anthony@anthonyui-macbook-pro.local>
2016-06-22 22:20:41 +0900

    Sample Qr generating~

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import QRCode
 
 class ViewController: UIViewController {
-
+    
+    let qrCodeName = QRCode("Anthony Kim")
+    @IBOutlet weak var qrImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        qrImage.image = qrCodeName?.image
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
