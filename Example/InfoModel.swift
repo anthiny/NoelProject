555c3dae2bc258472ef5342bad00a8040b07004b
n Anthony Kim <anthony@anthonyui-macbook-pro.local>
2016-06-23 01:03:55 +0900

    Basic Error Handling!

---
+++ Example/InfoModel.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+class ContactInfo {
+    let name: String
+    var phoneNumber: String?
+    var companyName: String
+    var email: String?
+    
+    init(name: String, phoneNumber: String?, companyName: String, email: String?)
+    {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.companyName = companyName
+        self.email = email
+    }
+    
+    func exchangeToQRString() -> String{
+        let QRString = "name$\(name)#phone$\(phoneNumber)#company$\(companyName)#email$\(email)#"
+        return QRString
+    }
+}
+
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -11,12 +11,19 @@ import QRCode
 
 class ViewController: UIViewController {
     
-    let qrCodeName = QRCode("Anthony Kim")
     @IBOutlet weak var qrImage: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var companyNameTextField: UITextField!
+    
+    enum InputError: ErrorType{
+        case noName
+        case OutOfRange
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        qrImage.image = qrCodeName?.image
+        
         // Do any additional setup after loading the view, typically from a nib.
         
     }
@@ -26,6 +33,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func generateButton(sender: AnyObject) {
+        do{
+            try generatingQRcode()
+        }catch InputError.noName{
+            print("No Name")
+        }catch InputError.OutOfRange{
+            print("OutOfRange")
+        }catch {
+            print("Exceptional Error!")
+        }
+    }
+    
+    func generatingQRcode() throws{
+        
+        guard nameTextField.text?.isEmpty == false else {
+            throw InputError.noName
+        }
+        
+        guard phoneNumberTextField.text?.characters.count < 11 else {
+            throw InputError.OutOfRange
+        }
+       
+        let Info = ContactInfo(name: nameTextField.text!, phoneNumber: phoneNumberTextField.text!, companyName: companyNameTextField.text!, email: nil)
+        var qrCodeContents = QRCode(Info.exchangeToQRString())
+        qrCodeContents!.backgroundColor = CIColor(rgba: "000")
+        qrImage.image=qrCodeContents?.image
+    }
+    
+    
 
 }
 
