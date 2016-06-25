32a740724e426c1b82b629c117c50ab93586a305
n Anthony Kim <anthony@anthonyui-macbook-pro.local>
2016-06-25 12:53:59 +0900

    Finish Table View Delete func!

--- Example/FirstViewContoller.swift
+++ Example/FirstViewContoller.swift
@@ -13,9 +13,11 @@ import UIKit
 class FirstViewController: UITableViewController {
     
     var persons = [NSManagedObject]()
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem()
 
     }
     
@@ -24,21 +26,48 @@ class FirstViewController: UITableViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
+        
+        coreDataFetch()
+        
+        self.tableView.reloadData()
+    }
     
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-
-        let managedContext = appDelegate.managedObjectContext
+    func coreDataFetch() {
         
         let fetchRequest = NSFetchRequest(entityName: "Info")
         
         do{
-            let results = try managedContext.executeFetchRequest(fetchRequest)
+            let results = try appDelegate.managedObjectContext.executeFetchRequest(fetchRequest)
             persons = results as! [NSManagedObject]
         } catch let error as NSError {
-            print("Could not fetch \(error), \(error.userInfo)")
+            print("Could not Load Core Data \(error), \(error.userInfo)")
         }
+    }
+    
+    func coreDataSave() {
         
-        self.tableView.reloadData()
+        do{
+            try appDelegate.managedObjectContext.save()
+        } catch let error as NSError{
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            let itemToDelete = persons[indexPath.row]
+            
+            appDelegate.managedObjectContext.deleteObject(itemToDelete)
+            
+            coreDataSave()
+            
+            coreDataFetch()
+            
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            
+        }
     }
     
     
