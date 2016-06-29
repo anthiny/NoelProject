0256dea20deb5242d45d1142d52b6ec1faea099d
n Anthony Kim <anthony@anthonyui-macbook-pro.local>
2016-06-29 15:41:40 +0900

    finished detail view & fixing searchbar issue!

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -9,19 +9,25 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var nameLabel: UILabel!
-    var text: String?
+    
+    var nameInfoLabel = UILabel()
+    var nameLabel = UILabel()
+    var phoneNumberInfoLabel = UILabel()
+    var phoneNumberLabel = UILabel()
+    var companyNameInfoLabel = UILabel()
+    var companyNameLabel = UILabel()
+    var emailInfoLabel = UILabel()
+    var emailLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setContentsConstrant()
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        nameLabel.text = text
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,15 +35,89 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setContentsConstrant(){
+        let superView = self.view
+        
+        superView.addSubview(nameInfoLabel)
+        superView.addSubview(nameLabel)
+        superView.addSubview(phoneNumberInfoLabel)
+        superView.addSubview(phoneNumberLabel)
+        superView.addSubview(companyNameInfoLabel)
+        superView.addSubview(companyNameLabel)
+        superView.addSubview(emailInfoLabel)
+        superView.addSubview(emailLabel)
+        
+        nameInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        phoneNumberInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        phoneNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        companyNameInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        companyNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        nameLabel.text = "Name"
+        nameLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(superView.snp_top).offset(30)
+            make.leading.equalTo(superView.snp_leading).offset(10)
+            make.width.equalTo(80)
+            make.height.equalTo(phoneNumberLabel.snp_height)
+        }
+        
+        phoneNumberLabel.text = "Phone"
+        phoneNumberLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(nameLabel.snp_bottom).offset(12)
+            make.leading.equalTo(superView.snp_leading).offset(10)
+            make.width.equalTo(nameLabel.snp_width)
+            make.height.equalTo(companyNameLabel.snp_height)
+        }
+        
+        companyNameLabel.text = "Company"
+        companyNameLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(phoneNumberLabel.snp_bottom).offset(12)
+            make.leading.equalTo(superView.snp_leading).offset(10)
+            make.width.equalTo(phoneNumberLabel.snp_width)
+            make.height.equalTo(emailLabel.snp_height)
+        }
+        
+        emailLabel.text = "Email"
+        emailLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(companyNameLabel.snp_bottom).offset(12)
+            make.leading.equalTo(superView.snp_leading).offset(10)
+            make.width.equalTo(companyNameLabel.snp_width)
+            make.bottom.equalTo(superView.snp_bottom).offset(-30)
+        }
+        
+        nameInfoLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(superView.snp_top).offset(30)
+            make.leading.equalTo(nameLabel.snp_trailing).offset(20)
+            make.trailing.equalTo(superView.snp_trailing).offset(-10)
+            make.height.equalTo(phoneNumberInfoLabel.snp_height)
+        }
+        
+        phoneNumberInfoLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(nameInfoLabel.snp_bottom).offset(12)
+            make.leading.equalTo(phoneNumberLabel.snp_trailing).offset(20)
+            make.trailing.equalTo(superView.snp_trailing).offset(-10)
+            make.height.equalTo(companyNameInfoLabel.snp_height)
+        }
+        
+        companyNameInfoLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(phoneNumberInfoLabel.snp_bottom).offset(12)
+            make.leading.equalTo(companyNameLabel.snp_trailing).offset(20)
+            make.trailing.equalTo(superView.snp_trailing).offset(-10)
+            make.height.equalTo(emailInfoLabel.snp_height)
+        }
+        
+        emailInfoLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(companyNameInfoLabel.snp_bottom).offset(12)
+            make.leading.equalTo(emailLabel.snp_trailing).offset(20)
+            make.trailing.equalTo(superView.snp_trailing).offset(-10)
+            make.bottom.equalTo(superView.snp_bottom).offset(-30)
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
 }
--- Example/FirstViewContoller.swift
+++ Example/FirstViewContoller.swift
@@ -19,9 +19,12 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
     let searchController = UISearchController(searchResultsController: nil)
     var filteredPersons = [NSManagedObject]()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = editButtonItem()
+        saveDummy()
+        //Add searchBar
         searchController.searchResultsUpdater = self
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.dimsBackgroundDuringPresentation = false
@@ -29,6 +32,7 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
         self.tableView.tableHeaderView = searchController.searchBar
     }
     
+    //SearchBar Filter
     func filterContentForSearchText(searchText: String, scope: String = "All") {
         filteredPersons = persons.filter { person in
             return (person.valueForKey("name")?.lowercaseString.containsString(searchText.lowercaseString))!
@@ -45,6 +49,7 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
         self.tableView.reloadData()
     }
     
+    //updating SearchResult
     func updateSearchResultsForSearchController(searchController: UISearchController) {
         filterContentForSearchText(searchController.searchBar.text!)
     }
@@ -105,6 +110,7 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
         }
     }
     
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if searchController.active && searchController.searchBar.text != "" {
@@ -113,6 +119,7 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
         return persons.count
     }
     
+    //Setting tableViewCell
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
         let person: NSManagedObject
@@ -126,20 +133,46 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
         return cell
     }
     
+    //Open DetailView
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         performSegueWithIdentifier("showDetail", sender: indexPath)
     }
     
+    //Perpare Segue
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if segue.identifier == "showDetail" {
             if let destinationVC = segue.destinationViewController as? DetailViewController{
+                searchController.active = false
                 let selectedRow = (sender as? NSIndexPath)?.row
-                let aa = persons[selectedRow!].valueForKey("name")
-                destinationVC.text = aa as? String
+                
+                if let name = persons[selectedRow!].valueForKey("name") as? String {
+                    destinationVC.nameInfoLabel.text = name
+                }else{
+                    print("name is null")
+                }
+                
+                if let name = persons[selectedRow!].valueForKey("phoneNumber") as? String {
+                    destinationVC.phoneNumberInfoLabel.text = name
+                }else{
+                    print("phoneNumber is null")
+                }
+                
+                if let name = persons[selectedRow!].valueForKey("companyName") as? String {
+                    destinationVC.companyNameInfoLabel.text = name
+                }else{
+                    print("companyName is null")
+                }
+                
+                if let name = persons[selectedRow!].valueForKey("email") as? String {
+                    destinationVC.emailInfoLabel.text = name
+                }else{
+                    print("email is null")
+                }
             }
         }
     }
     
+    //When QRreader read QRcode
     func reader(reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
         let resultStrings = result.value.componentsSeparatedByString("$")
         if resultStrings.count != 4 {
@@ -153,10 +186,30 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
         }
     }
     
+    //Cancel button func which is in the QRScan View
     func readerDidCancel(reader: QRCodeReaderViewController) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    //make a dummy
+    func saveDummy()
+    {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        let entity = NSEntityDescription.entityForName("Info", inManagedObjectContext: managedContext)
+        
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        person.setValue("lee", forKey: "name")
+        person.setValue("01042413030", forKey: "phoneNumber")
+        person.setValue("사람", forKey: "companyName")
+        person.setValue("aaa@naver.com", forKey: "email")
+
+    }
+    
+    //save to coredata
     func saveInfo(info: ContactInfo, reader: QRCodeReaderViewController) {
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
@@ -171,7 +224,6 @@ class FirstViewController: UITableViewController, QRCodeReaderViewControllerDele
         person.setValue(info.phoneNumber, forKey: "phoneNumber")
         person.setValue(info.companyName, forKey: "companyName")
         person.setValue(info.email, forKey: "email")
-        print(person.valueForKey("phoneNumber"))
         do{
             try managedContext.save()
             let alert = UIAlertController(title: "Success Add New Item!", message: "Add \(info.name)'s Info", preferredStyle: .Alert)
